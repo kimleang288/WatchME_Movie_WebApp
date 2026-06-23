6f4dce163d7851f71487bc100f3a15df0fdc07c5
kimleang288 <bbottra4@gmail.com>
2026-06-23 08:49:18 +0700

    Project

--- watchme-app/vite.config.js
+++ watchme-app/vite.config.js
@@ -1,10 +1,26 @@
-import { defineConfig } from 'vite';
+﻿import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/css/about.css',
+                'resources/css/home.css',
+                'resources/js/home.js',
+                'resources/css/movie-detail.css',
+                'resources/js/movie-detail.js',
+                'resources/css/search-results.css',
+                'resources/js/tv-detail.js',
+                'resources/css/auth/register.css',
+                'resources/js/auth/register.js',
+                'resources/css/auth/forgot-password.css',
+                'resources/css/auth/login.css',
+                'resources/css/auth/reset-password.css',
+                'resources/js/auth/reset-password.js',
+            ],
             refresh: true,
         }),
     ],
